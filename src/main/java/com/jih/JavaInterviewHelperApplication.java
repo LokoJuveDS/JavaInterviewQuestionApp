3cd33b567a0c288a0d37357fbf5e89a85693f0bb
Yegor Priimak <egorpryimak@gmail.com>
2026-05-13 13:29:20 +0200

    Migrate to Spring Boot and rename application

---
+++ src/main/java/com/jih/JavaInterviewHelperApplication.java
@@ -0,0 +1,11 @@
+package com.jih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInterviewHelperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInterviewHelperApplication.class, args);
+    }
+}
